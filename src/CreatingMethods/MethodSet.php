3dc1ff1eb058d31de84f74464080402999d37357
Vladimir <elcopro@gmail.com>
2022-10-27 11:48:45 +0300

    MethodSet

---
+++ src/CreatingMethods/MethodSet.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Last1971\SmartCommand\CreatingMethods;
+
+use Last1971\SmartCommand\Interfaces\IMethodable;
+use ReflectionMethod;
+use ReflectionNamedType;
+use ReflectionParameter;
+
+class MethodSet implements IMethodable
+{
+
+    /**
+     * @param ReflectionMethod $method
+     * @param array $args
+     * @return string
+     */
+    public function createMethod(ReflectionMethod $method, ...$args): string
+    {
+        $methodName = $method->getName();
+        $name = lcfirst(substr($methodName, 3));
+        $parameter = $method->getParameters()[0];
+        $response = "public function $methodName(";
+        $response .= $this->createParameter($parameter);
+        $response .= "): void { ";
+        $parameterName = $parameter->getName();
+        $response .= "\$this->uObject->set('$name', \$$parameterName); }";
+        return $response;
+    }
+
+    /**
+     * @param ReflectionParameter $parameter
+     * @return string
+     */
+    private function createParameter(ReflectionParameter $parameter): string
+    {
+        $type = $parameter->getType();
+        $returnTypeName = $type instanceof ReflectionNamedType ? $type->getName() : '';
+        $name = $parameter->getName();
+        $response = "$returnTypeName $$name";
+        if ($parameter->isDefaultValueAvailable()) {
+            $defaultValue = $parameter->getDefaultValue();
+            $response .= "=$defaultValue";
+        }
+        return $response;
+    }
+}
---
+++ tests/MethodSetTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests;
+
+use Last1971\SmartCommand\CreatingMethods\MethodSet;
+use Last1971\SmartCommand\Interfaces\IUObject;
+use PHPUnit\Framework\TestCase;
+use ReflectionMethod;
+use ReflectionParameter;
+
+class MethodSetTest extends TestCase
+{
+    public function testOk(): void
+    {
+        $method = $this->createMock(ReflectionMethod::class);
+        $parameter = $this->createMock(ReflectionParameter::class);
+        $parameter->method('getType')->willReturn(null);
+        $parameter->method('getName')->willReturn('someString');
+        $method->method('getName')->willReturn('setSomeString');
+        $method->method('getParameters')->willReturn([$parameter]);
+        $methodSet = new MethodSet();
+        $response = $methodSet->createMethod($method);
+        $this->assertEquals(
+            'public function setSomeString( $someString): void { $this->uObject->set(\'someString\', $someString); }',
+            $response
+        );
+    }
+}
